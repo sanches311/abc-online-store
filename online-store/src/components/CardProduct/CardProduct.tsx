9cd2a353582ae1d8311cdff937db311717ef04fb
sanches311 <itchipset@gmail.com>
2023-10-01 19:08:05 +0300

    feat: add heart to ProductCard

--- online-store/src/components/CardProduct/CardProduct.tsx
+++ online-store/src/components/CardProduct/CardProduct.tsx
@@ -5,6 +5,7 @@ import RatingProduct from '../RatingProduct/RatingProduct';
 import { NavLink } from 'react-router-dom';
 import { useAppDispatch } from '../../hooks/redux';
 import { addProductToCart } from '../../store/userSlice';
+import heart from '../../assets/icons/heart.svg';
 
 type Props = {
   product: IProduct;
@@ -29,6 +30,21 @@ const CardProduct: React.FC<Props> = ({ product }: Props) => {
     <NavLink to={`products/${product.id}`}>
       <div className={classes.card}>
         <li className={classes.wrapper_img}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="fill"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={classes.heartIcon}
+            onClick={(e) => e.preventDefault()}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+            />
+          </svg>
           <img src={product.image} alt="image" />
         </li>
         <li className={classes.wrapper_rating}>
