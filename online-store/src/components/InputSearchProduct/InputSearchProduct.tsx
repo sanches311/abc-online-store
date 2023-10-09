b925052527ddf9ae5db7b84ce4d1a7419ae3cebd
sanches311 <itchipset@gmail.com>
2023-10-09 16:34:46 +0300

    fix: search in InputSearchProduct

--- online-store/src/components/InputSearchProduct/InputSearchProduct.tsx
+++ online-store/src/components/InputSearchProduct/InputSearchProduct.tsx
@@ -4,26 +4,28 @@ import { useSearchParams } from 'react-router-dom';
 
 const InputSearchProduct = () => {
   const [searchParams, setSearchParams] = useSearchParams();
-  const query = searchParams.get('query') ?? '';
   const params = new URLSearchParams();
 
-  const handleOnChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (searchParams.has('limit')) params.set('limit', searchParams.get('limit') ?? '');
-    if (searchParams.has('sort')) params.set('sort', searchParams.get('sort') ?? '');
-    if (!e.target.value) {
-      params.delete('query');
-    } else {
-      params.set('query', e.target.value);
+  const handleOnKeyDownSearch = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      const target = e.currentTarget;
+      if (searchParams.has('limit')) params.set('limit', searchParams.get('limit') ?? '');
+      if (searchParams.has('sort')) params.set('sort', searchParams.get('sort') ?? '');
+      if (!target.value) {
+        params.delete('query');
+      } else {
+        params.set('query', target.value);
+      }
+      target.value = '';
+      setSearchParams(params);
     }
-    setSearchParams(params);
   };
   return (
     <input
       className={classes.search}
       type="search"
       placeholder="Search..."
-      value={query}
-      onChange={handleOnChangeSearch}
+      onKeyDown={handleOnKeyDownSearch}
     />
   );
 };
