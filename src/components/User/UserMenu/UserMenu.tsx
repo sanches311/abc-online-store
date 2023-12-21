56b7f3fef5225e7742c95aa008c704411f6d7758
Alexander Naumenok <itchipset@gmail.com>
2023-12-21 11:25:50 +0300

    fix: UserMenu

--- src/components/User/UserMenu/UserMenu.tsx
+++ src/components/User/UserMenu/UserMenu.tsx
@@ -2,22 +2,34 @@ import React, { useRef } from 'react';
 import classes from './UserMenu.module.scss';
 import { useAppDispatch, useAppSelector } from '../../../hooks/redux';
 import {
+  delCurrentUser,
   toggleVisibleUserLoginForm,
   toggleVisibleUserMenu,
   toggleVisibleUserSignInForm,
   useGetUserQuery,
 } from '../../../store/userSlice';
 import useOnClickOutside from '../../../hooks/onClickOutSide';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { changesDisplayPrice } from '../../../utils/utils';
+import ClockSVG from '../../../assets/icons/clock.svg';
+import ShoppingBagSVG from '../../../assets/icons/shopping-bag.svg';
+import HeartSVG from '../../../assets/icons/heart.svg';
 
 const UserMenu: React.FC = () => {
   const userMenu = useRef(null);
   const userId = useAppSelector((state) => state.user.currentUserId);
+  const bag = useAppSelector((state) => state.user.cart);
+  const wishList = useAppSelector((state) => state.user.wishlist.length);
+  const countProducts = changesDisplayPrice(
+    bag.reduce((sum, product) => sum + product.quantity, 0)
+  );
 
   const dispatch = useAppDispatch();
   const visible = useAppSelector((state) => state.user.userMenu);
   const id = useAppSelector((state) => state.user.currentUserId);
   const { data } = useGetUserQuery(id, { skip: id === null });
+
+  const navigate = useNavigate();
 
   const showLogin = () => {
     dispatch(toggleVisibleUserLoginForm(true));
@@ -47,9 +59,12 @@ const UserMenu: React.FC = () => {
   const handleOnClickOrders = () => {
     if (!userId) {
       showLogin();
-    }
+    } else navigate(`/orders/user/${userId}`);
 
     hideUserMenu();
+  };
+  const logOut = () => {
+    dispatch(delCurrentUser());
   };
   useOnClickOutside(userMenu, hideUserMenu, visible);
   return (
@@ -58,12 +73,41 @@ const UserMenu: React.FC = () => {
       className={visible ? `${classes.wrapper} ${classes.active}` : `${classes.wrapper}`}
     >
       <ul className={classes.wrapper_menu}>
-        <li>Hello, {id === null ? 'Guest' : `${data?.username}`}! </li>
-        <Link to={`/orders/user/${userId}`}>
-          <li onClick={handleOnClickOrders}>My orders</li>
-        </Link>
-        <li onClick={handleOnClickLogin}>Log in</li>
-        <li onClick={handleOnClickSignIn}>Sign in</li>
+        <li>
+          <span className={classes.text_bold}>
+            Hello, {id === null ? 'Guest' : `${data?.username}`}!
+          </span>
+          <span className={classes.link} onClick={logOut}>
+            Log out
+          </span>
+        </li>
+        <li onClick={handleOnClickOrders}>
+          <ClockSVG />
+          <span className={classes.link}>My order history</span>
+        </li>
+
+        <li>
+          <ShoppingBagSVG />
+          <Link to="/bag">
+            <span className={classes.link} onClick={hideUserMenu}>
+              {countProducts} products in bag
+            </span>
+          </Link>
+        </li>
+        <li>
+          <HeartSVG />
+          <Link to="/wishlist">
+            <span className={classes.link} onClick={hideUserMenu}>
+              {wishList} products in wishList
+            </span>
+          </Link>
+        </li>
+        <li onClick={handleOnClickLogin} className={classes.link}>
+          Log in
+        </li>
+        <li onClick={handleOnClickSignIn} className={classes.link}>
+          Sign in
+        </li>
       </ul>
     </div>
   );
